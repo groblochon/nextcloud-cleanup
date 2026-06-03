a5659a24c2297fe99e666489d90de97ff15bffd0
picman <picman@nos.bzh>
2026-06-03 10:44:37 +0200

    Catch and log error

--- nextcloud_scan.py
+++ nextcloud_scan.py
@@ -75,7 +75,7 @@ def get_all_fileids(conn):
 def test_object_via_occ(urn_oid):
     try:
         # Utilisation de lib_nc-occ via _process pour passer l'argument manquant
-        output = files_api._process(["files:object:info", urn_oid])
+        output = files_api._process(["files:object:get", urn_oid])
         lower = output.response_str.lower()
         print(lower)
 
