a9fe9f8536e0149f2d7098114684231a2a76e0fd
picman <picman@nos.bzh>
2026-06-03 10:21:06 +0200

    Catch and log error

--- nextcloud_scan.py
+++ nextcloud_scan.py
@@ -18,7 +18,7 @@ NC_PATH = '/var/www/nextcloud'
 WEB_USER = 'nextcloud'
 
 files_api = nc_occ.Files()
-files_api.scan("--all")
+files_api._process(["scan", "--all"], False)
 maintenance_api = nc_occ.Maintenance()
 
 def connect_db():
